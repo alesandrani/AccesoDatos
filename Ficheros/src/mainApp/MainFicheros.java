cafa5f68338aed7d100d3ddfde1286334aec5b40
Aleksandra Smorodina <aleksandra.smorodina@bootcamp-upgrade.com>
2024-10-08 16:09:47 +0200

    agregar fichero

---
+++ Ficheros/src/mainApp/MainFicheros.java
@@ -0,0 +1,12 @@
+package mainApp;
+
+import interfaz.InterfazUsuario;
+
+public class MainFicheros {
+
+	public static void main(String[] args) {
+		new InterfazUsuario().mostrarInterfaz();
+		
+	}
+
+}
